e7ec55d14effb5bc7bbdc85c4e66a31b836ca074
Mischosch <michael@sch0ll.de>
2011-09-03 06:29:22 +0200

    - add Buggy/Application/Resource/Doctrine.php

---
+++ library/Buggy/Application/Resource/Doctrine.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Buggy\Application\Resource;
+
+use Zend\Application\Resource\AbstractResource as AbstractResource, 
+	Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Configuration, 
+    Zend\Registry;
+
+class Doctrine extends AbstractResource
+{
+	/**
+	 * @var Doctrine\ORM\EntityManager
+	 */
+	protected $entityManager;
+	
+	/**
+	 * @var array
+	 */
+	protected $_options = array(
+		'connection' => array(
+			'driver' => 'pdo_mysql', 
+			'host' => 'localhost', 
+			'dbname' => 'dbname', 
+			'user' => 'root', 
+			'password' => ''
+		),
+		'cacheImplementation' => '\Doctrine\Common\Cache\ArrayCache', 
+		'modelDir' => '/models',
+		'proxyDir' => '/proxies',
+		'proxyNamespace' => 'Proxies',
+		'autoGenerateProxyClasses' => true
+	);
+
+	public function init()
+	{
+		$options = $this->getOptions();
+		$config = new Configuration;
+		$cache = new $options['cacheImplementation'];
+		$driverImpl = $config->newDefaultAnnotationDriver($options['modelDir']);
+		
+		$config->setMetadataDriverImpl($driverImpl);
+		$config->setMetadataCacheImpl($cache);
+		$config->setQueryCacheImpl($cache);
+		$config->setProxyDir($options['proxyDir']);
+		$config->setProxyNamespace($options['proxyNamespace']);
+		$config->setAutoGenerateProxyClasses($options['autoGenerateProxyClasses']);
+
+		$this->entityManager = EntityManager::create($options['connection'], $config);
+		Registry::set('em', $this->entityManager);
+
+		return $this->entityManager;
+	}
+}
