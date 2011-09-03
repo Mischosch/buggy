a3788e98ab0c1c6235f083e598d5936e2f2eba31
Mischosch <michael@sch0ll.de>
2011-09-03 06:21:43 +0200

    - Doctrine EntityManager Integration - application.ini changes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,41 @@
 <?php
 
-class Bootstrap extends \Zend\Application\Bootstrap
+use Buggy\Application\Resource\Doctrine, 
+	Zend\Application\Bootstrap as BaseBootstrap;
+	
+class Bootstrap extends BaseBootstrap
 {
 
+	/**
+	 * Init Doctrine EntityManager
+	 * 
+	 * @return Doctrine\ORM\EntityManager
+	 */
+	protected function _initDoctrine()
+    {
+    	$options = $this->getOption('resources');
+    	$doctrine = new Doctrine($options['doctrine']);
+    	$doctrine->init();
+    	return $doctrine;
+    }
+    
+    /**
+	 * Init View Options
+	 * 
+	 * @return void
+	 */
+	protected function _initViewOptions()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->broker('doctype')->setDoctype('HTML5');
+        $view->broker('headTitle')->setSeparator(' | ')
+        	->prepend('Buggy');
+        $view->broker('headScript')->appendFile('/js/vendor/jquery.min.js');
+        $view->broker('headLink')->appendStylesheet(
+        	'http://twitter.github.com/bootstrap/assets/css/bootstrap-1.1.1.min.css'
+        );
+    }
 
 }
 
