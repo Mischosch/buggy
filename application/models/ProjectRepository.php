5b0c5f4ccabf81c0d9d9ff7f69bde08cc5e513d1
Mischosch <michael@sch0ll.de>
2011-09-05 04:26:53 +0200

    add getProjectList method

--- application/models/ProjectRepository.php
+++ application/models/ProjectRepository.php
@@ -7,6 +7,20 @@ use Doctrine\ORM\EntityRepository;
 class ProjectRepository extends EntityRepository
 {
 	/**
+	 * Get Project List
+	 * 
+	 * @return array
+	 */
+	public function getProjectList() {
+	    $qb = $this->_em->createQueryBuilder();
+	    $qb->select('p')
+	        ->from('Application\Model\Project', 'p')
+	        ->orderBy('p.title');
+	        
+    	return $qb->getQuery()->getResult();
+	}
+	
+	/**
 	 * Find Projects by title
 	 * 
 	 * @param string $description
@@ -15,7 +29,7 @@ class ProjectRepository extends EntityRepository
 	public function findActiveProjectsByTitle($title) {
 	    $qb = $this->_em->createQueryBuilder();
 	    $qb->select('p')
-	        ->from('Application\Model\Projects', 'p')
+	        ->from('Application\Model\Project', 'p')
 	        ->where('p.title = :title')
 	        ->setParameter('title', $title)
 	        ->orderBy('p.title');
