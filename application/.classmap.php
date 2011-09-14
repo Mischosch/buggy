d4b6b941374d48f96506595a4de353f804e4d06c
Mischosch <michael@sch0ll.de>
2011-09-15 00:11:22 +0200

    reduce classmap

---
+++ application/.classmap.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'Bootstrap' => __DIR__ . DIRECTORY_SEPARATOR . 'Bootstrap.php',
+  'Application\\Document\\Article' => __DIR__ . DIRECTORY_SEPARATOR . 'documents' . DIRECTORY_SEPARATOR . 'Article.php',
+  'Application\\Document\\Tag' => __DIR__ . DIRECTORY_SEPARATOR . 'documents' . DIRECTORY_SEPARATOR . 'Tag.php',
+  'Application\\Model\\Project' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Project.php',
+  'Application\\Model\\ProjectRepository' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'ProjectRepository.php',
+  'Application\\Model\\Users' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Users.php',
+  'Application\\Model\\UsersRepository' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'UsersRepository.php',
+);
