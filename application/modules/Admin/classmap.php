6fe84b924c69d4f20e984364343cb61167277fd6
Mischosch <michael@sch0ll.de>
2011-09-18 15:58:06 +0200

    add new stuff to classmaps

--- application/modules/Admin/classmap.php
+++ application/modules/Admin/classmap.php
@@ -3,5 +3,7 @@ return array (
   'Admin\\Controller\\OverviewController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Admin' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'OverviewController.php',
   'Admin\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Admin' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
   'Admin\\Controller\\ProjectsController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Admin' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'ProjectsController.php',
+  'Admin\\Form\\Project' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Admin' . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR . 'Project.php',
+  'Admin\\Service\\Projects' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Admin' . DIRECTORY_SEPARATOR . 'Service' . DIRECTORY_SEPARATOR . 'Projects.php',
   'Admin\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
 );
--- application/modules/Buggy/classmap.php
+++ application/modules/Buggy/classmap.php
@@ -1,11 +1,16 @@
 <?php
 return array (
-  'Buggy\\View\\Helper\\Url' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Url.php',
   'Buggy\\Bootstrap' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR . 'Bootstrap.php',
   'Buggy\\Action\Helper\\BaseInit' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Action' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'BaseInit.php',
   'Buggy\\Resource\\DoctrineEntityManager' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Resource' . DIRECTORY_SEPARATOR . 'DoctrineEntityManager.php',
   'Buggy\\Resource\\CouchDocumentManager' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Resource' . DIRECTORY_SEPARATOR . 'CouchDocumentManager.php',
   'Buggy\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
   'Buggy\\Controller\\ErrorController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'ErrorController.php',
+  'Buggy\\Form\\AbstractForm' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR . 'AbstractForm.php',
+  'Buggy\\Form\\Decorator' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR . 'Decorator.php',
+  'Buggy\\Form\\Decorator\\BootstrapErrors' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR . 'Decorator' . DIRECTORY_SEPARATOR . 'BootstrapErrors.php',
+  'Buggy\\Form\\Decorator\\BootstrapTag' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR . 'Decorator' . DIRECTORY_SEPARATOR . 'BootstrapTag.php',
+  'Buggy\\View\\Helper\\Url' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Url.php',
+  'Buggy\\View\\Helper\\MessagesFormatter' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Buggy' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'MessagesFormatter.php',
   'Buggy\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
 );
