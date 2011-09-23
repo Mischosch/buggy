6c0e20eba7fd5af5fc48f5ded270549131542555
Mischosch <michael@sch0ll.de>
2011-09-23 17:49:06 +0200

    remove old classmap

--- application/.classmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'Application\\Document\\Article' => __DIR__ . DIRECTORY_SEPARATOR . 'documents' . DIRECTORY_SEPARATOR . 'Article.php',
-  'Application\\Document\\Tag' => __DIR__ . DIRECTORY_SEPARATOR . 'documents' . DIRECTORY_SEPARATOR . 'Tag.php',
-  'Application\\Model\\Project' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Project.php',
-  'Application\\Model\\ProjectRepository' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'ProjectRepository.php',
-  'Application\\Model\\Users' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Users.php',
-  'Application\\Model\\UsersRepository' => __DIR__ . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'UsersRepository.php',
-);
