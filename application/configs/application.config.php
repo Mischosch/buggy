8a976c8c0ba0acf0e823e091b7d826e6061f009a
Mischosch <michael@sch0ll.de>
2011-10-01 05:20:22 +0200

    remove ZendModule/ZendMvc from modules setting

--- application/configs/application.config.php
+++ application/configs/application.config.php
@@ -1,17 +1,14 @@
 <?php
 return new Zend\Config\Config(array(
     'module_paths' => array(
-        APPLICATION_PATH . '/modules', 
-        realpath(__DIR__ . '/../../modules'),
+        APPLICATION_PATH . '/modules',
     ),
     'modules' => array(
-        'ZendModule', // @todo: really need it?
-        'ZendMvc',
         'Buggy',
         'Admin',
     ),
     'module_config' => array( 
         'cache_config'  => false,
-        'cache_dir'     => realpath(__DIR__ . '/../../data/cache'),
+        'cache_dir'     => BASE_PATH . '/data/cache',
     ),
 ));
