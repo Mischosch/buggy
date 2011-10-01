8f993245c43133170ed703d23503a1cb731af014
Mischosch <michael@sch0ll.de>
2011-10-01 05:18:01 +0200

    code cleaning

--- application/modules/Admin/src/Admin/Controller/ProjectsController.php
+++ application/modules/Admin/src/Admin/Controller/ProjectsController.php
@@ -4,124 +4,141 @@ namespace Admin\Controller;
 
 use Zend\Debug;
 
-use Buggy\Model\Project,
-	Admin\Service\Projects, 
-	Zend\Filter\Digits, 
-	Zend\Controller\Action\Helper\FlashMessenger,
-	Buggy\Resource\DoctrineEntityManager as DoctrineEntityManager, 
-	Zend\Mvc\Controller\ActionController, 
-	Zend\Mvc\Router\RouteStack;
+use Buggy\Model\Project, 
+    Admin\Service\Projects, 
+    Zend\Filter\Digits, 
+    Zend\Controller\Action\Helper\FlashMessenger, 
+    Buggy\Resource\DoctrineEntityManager as DoctrineEntityManager, 
+    Zend\Mvc\Controller\ActionController, 
+    Zend\Mvc\Router\RouteStack;
 
 class ProjectsController extends ActionController
 {
 
-	/**
-	 * @var Buggy\Resource\DoctrineEntityManager
-	 */
-	protected $em;
-	
-	/**
-	 * @var Admin\Service\Projects
-	 */
-	protected $projectService;
-	
-	/**
-	 * @var Zend\Controller\Action\Helper\FlashMessenger
-	 */
-	protected $flashMessenger;
-	
+    /**
+     * @var Buggy\Resource\DoctrineEntityManager
+     */
+    protected $em;
 
+    /**
+     * @var Admin\Service\Projects
+     */
+    protected $projectService;
+
+    /**
+     * @var Zend\Controller\Action\Helper\FlashMessenger
+     */
+    protected $flashMessenger;
+
+    public function init()
+    {
+        exit("dfsdf");
+    }
+    
     public function indexAction()
     {
-    	$projects = $this->em->getRepository('Buggy\Model\Project')
-    		->getProjectList(); //->findAll();
-   	    return array(
-   	    	'projects' => $projects, 
-   	    	'title'    => 'Projects',
-   	    	'messages' => $this->getFlashMessenger('projects')->getMessages()
-   	    );
+        $projects = $this->em->getRepository('Buggy\Model\Project')->getProjectList(); //->findAll();
+        return array(
+            'projects' => $projects, 
+            'title' => 'Projects', 
+            'messages' => $this->getFlashMessenger('projects')->getMessages()
+        );
     }
-    
-	public function addAction()
+
+    public function addAction()
     {
-    	$form = $this->projectService->getProjectForm();
-    	$messages = false;
-    	if ($this->getRequest()->isPost()) {
-    		if ($this->getRequest()->post()->offsetExists('cancel')) {
-    			return $this->redirect('projects', 'index');
-    		}
-    		if ($form->isValid($this->getRequest()->post()->toArray())) {
-    			$values = $form->getValues();
-    			$projectRecord = new Project();
-    			$projectRecord->setTitle($values['title']);
-    			$projectRecord->setDescription($values['description']);
-    			$projectRecord->setPublic($values['public']);
-		    	$this->em->persist($projectRecord);
-		    	$this->em->flush();
-		    	$this->getFlashMessenger('projects')->addMessage(array('success', 'Project: <em>'.$projectRecord->getTitle().'</em> added succesfully!'));
-    			return $this->redirect('projects', 'index');
-    		} else {
-    			$messages = array('error', 'Please controll your input!');
-    			$form->buildBootstrapErrorDecorators();
-    		}
-    	}
-    		
-	    return array(
-	    	'title'    => 'Add Project',
-	    	'form'     => $form,
-    		'messages' => $messages
-    	);
-    	
-    }
+        $form = $this->projectService->getProjectForm();
+        $messages = false;
+        if ($this->getRequest()->isPost()) {
+            if ($this->getRequest()->post()->offsetExists('cancel')) {
+                return $this->redirect('projects', 'index');
+            }
+            if ($form->isValid($this->getRequest()->post()->toArray())) {
+                $values = $form->getValues();
+                $projectRecord = new Project();
+                $projectRecord->setTitle($values['title']);
+                $projectRecord->setDescription($values['description']);
+                $projectRecord->setPublic($values['public']);
+                $this->em->persist($projectRecord);
+                $this->em->flush();
+                $this->getFlashMessenger('projects')->addMessage(
+                        array(
+                            'success', 
+                            'Project: <em>' . $projectRecord->getTitle() . '</em> added succesfully!'
+                        ));
+                return $this->redirect('projects', 'index');
+            } else {
+                $messages = array(
+                    'error', 
+                    'Please controll your input!'
+                );
+                $form->buildBootstrapErrorDecorators();
+            }
+        }
+        
+        return array(
+            'title' => 'Add Project', 
+            'form' => $form, 
+            'messages' => $messages
+        );
     
-	public function editAction()
+    }
+
+    public function editAction()
     {
-    	$filter = new Digits();
-    	$projectId = $filter->filter($this->getQueryParam('id'));
-    	if (is_numeric($projectId)) {
-	    	$projectRecord = $this->em->find('Buggy\Model\Project', $projectId);
-	    	if ($projectRecord) {
-	    		$form = $this->projectService->getProjectForm();
-	    		$form->setAction('/admin/projects/edit/id/'.$projectId);
-	    		$form->setValues($projectRecord);
-	    		$messages = false;
-	    		if ($this->getRequest()->isPost()) {
-	    			if ($this->getRequest()->post()->offsetExists('cancel')) {
-	    				return $this->redirect('projects', 'index');
-	    			}
-	    			if ($form->isValid($this->getRequest()->post()->toArray())) {
-	    				$values = $form->getValues();
-	    				$projectRecord->setTitle($values['title']);
-	    				$projectRecord->setDescription($values['description']);
-	    				$projectRecord->setPublic($values['public']);
-				    	$this->em->persist($projectRecord);
-				    	$this->em->flush();
-				    	$this->getFlashMessenger('projects')->addMessage(array('success', 'Project: <em>'.$projectRecord->getTitle().'</em> saved succesfully!'));
-	    				return $this->redirect('projects', 'index');
-	    			} else {
-	    				$messages = array('error', 'Please controll your input!');
-	    				$form->buildBootstrapErrorDecorators();
-	    			}
-	    		}
-	    		
-		    	return array(
-		    		'title'    => 'Edit Project',
-		    		'form'     => $form,
-		    		'messages' => $messages
-		    	);
-	    	}
-    	}
-    	return $this->redirect('projects', 'index');
+        $filter = new Digits();
+        $projectId = $filter->filter(
+                $this->getEvent()->getRouteMatch()->getParam('id'));
+        if (is_numeric($projectId)) {
+            $projectRecord = $this->em->find('Buggy\Model\Project', $projectId);
+            if ($projectRecord) {
+                $form = $this->projectService->getProjectForm();
+                $form->setAction('/admin/projects/edit/id/' . $projectId);
+                $form->setValues($projectRecord);
+                $messages = false;
+                if ($this->getRequest()->isPost()) {
+                    if ($this->getRequest()->post()->offsetExists('cancel')) {
+                        return $this->redirect('projects', 'index');
+                    }
+                    if ($form->isValid($this->getRequest()->post()->toArray())) {
+                        $values = $form->getValues();
+                        $projectRecord->setTitle($values['title']);
+                        $projectRecord->setDescription($values['description']);
+                        $projectRecord->setPublic($values['public']);
+                        $this->em->persist($projectRecord);
+                        $this->em->flush();
+                        $this->getFlashMessenger('projects')->addMessage(
+                                array(
+                                    'success', 
+                                    'Project: <em>' . $projectRecord->getTitle() . '</em> saved succesfully!'
+                                ));
+                        return $this->redirect('projects', 'index');
+                    } else {
+                        $messages = array(
+                            'error', 
+                            'Please controll your input!'
+                        );
+                        $form->buildBootstrapErrorDecorators();
+                    }
+                }
+                
+                return array(
+                    'title' => 'Edit Project', 
+                    'form' => $form, 
+                    'messages' => $messages
+                );
+            }
+        }
+        return $this->redirect('projects', 'index');
     }
-    
-	public function testAction()
+
+    public function testAction()
     {
-    	Debug::dump($this->em->getRepository('Buggy\Model\Project')
-    		->findActiveProjectsByTitle('Hallo Welt'));
-    	exit;
+        Debug::dump($this->em->getRepository('Buggy\Model\Project')
+                    ->findActiveProjectsByTitle('Hallo Welt'));
+        exit();
     }
-    
-    
+
     /**
      * Redirect to a controller/action combo by using admin route
      * 
@@ -129,24 +146,30 @@ class ProjectsController extends ActionController
      * @param  string $action
      * @return Zend\Http\Response
      */
-	protected function redirect($controller, $action)
+    protected function redirect($controller, $action)
     {
         $redirect = $this->router->assemble(
-            array('controller' => $controller, 'action' => $action, 'id' => null), 
-            array('name' => 'admin', 'reset' => true)
-        );
+                array(
+                    'controller' => $controller, 
+                    'action' => $action, 
+                    'id' => null
+                ), 
+                array(
+                    'name' => 'admin', 
+                    'reset' => true
+                ));
         $this->response->setStatusCode(302);
         $this->response->headers()->addHeaderLine('Location', $redirect);
         return $this->response;
     }
-    
+
     /**
      * Get Flash Messenger Instance
      * 
      * @param  string $namespace
      * @return Zend\Controller\Action\Helper\FlashMessenger
      */
-	public function getFlashMessenger($namespace = false)
+    public function getFlashMessenger($namespace = false)
     {
         if (!$this->flashMessenger) {
             $this->flashMessenger = new FlashMessenger();
@@ -156,32 +179,32 @@ class ProjectsController extends ActionController
         }
         return $this->flashMessenger;
     }
-    
+
     /**
      * @param Buggy\Resource\DoctrineEntityManager $em
      * @return Admin\Controller\ProjectsController
      */
-	public function setDoctrineEntityManager(DoctrineEntityManager $em)
+    public function setDoctrineEntityManager(DoctrineEntityManager $em)
     {
         $this->em = $em->getEntityManager();
         return $this;
     }
-    
+
     /**
      * @param Admin\Service\Projects $projectService
      * @return Admin\Controller\ProjectsController
      */
-	public function setProjectsService(Projects $projectService)
+    public function setProjectsService(Projects $projectService)
     {
         $this->projectService = $projectService;
         return $this;
     }
-    
+
     /**
      * @param Zend\Mvc\Router\RouteStack $router
      * @return Admin\Controller\ProjectsController
      */
-	public function setRouter(RouteStack $router)
+    public function setRouter(RouteStack $router)
     {
         $this->router = $router;
         return $this;
