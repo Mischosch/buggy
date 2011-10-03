eeccd08f6f55184febecdf35897140e41c68a316
Mischosch <michael@sch0ll.de>
2011-10-04 01:38:57 +0200

    LocatorAware example

--- application/modules/Buggy/src/Buggy/Controller/IndexController.php
+++ application/modules/Buggy/src/Buggy/Controller/IndexController.php
@@ -2,14 +2,18 @@
 
 namespace Buggy\Controller;
 
+use Zend\Mvc\LocatorAware;
+
 use Zend\Mvc\Controller\ActionController,
+    Zend\Di\Locator,
 	Buggy\Resource\DoctrineEntityManager as DoctrineEntityManager;
 
-class IndexController extends ActionController
+class IndexController extends ActionController implements LocatorAware
 {
 	protected $em;
+	protected $locator;
 
-    public function indexAction()
+	public function indexAction()
     {
     	return array('test' => 'IT WORKS!');
     }
@@ -18,6 +22,22 @@ class IndexController extends ActionController
     {
         $this->em = $em;
         return $this;
+    }
+    
+	/**
+     * @return the $locator
+     */
+    public function getLocator()
+    {
+        return $this->locator;
+    }
+
+	/**
+     * @param Zend\Di\Locator $locator
+     */
+    public function setLocator(Locator $locator)
+    {
+        $this->locator = $locator;
     }
 
 }
