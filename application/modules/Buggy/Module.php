268dd7d3ddc766865231e4cb06f1dbf070eb8000
Mischosch <michael@sch0ll.de>
2011-10-03 19:29:22 +0200

    integrate registerApplicationListeners & registerStaticListeners methods

--- application/modules/Buggy/Module.php
+++ application/modules/Buggy/Module.php
@@ -2,12 +2,20 @@
 
 namespace Buggy;
 
-use Zend\Config\Config,
+use InvalidArgumentException,
+    Zend\Module\Manager,
+    Zend\Config\Config,
+    Zend\Di\Locator,
+    Zend\EventManager\EventCollection,
+    Zend\EventManager\StaticEventCollection,
     Zend\Loader\AutoloaderFactory;
 
 class Module
 {
-	
+    protected $appListeners    = array();
+    protected $staticListeners = array();
+    protected $viewListener;
+    
 	public function init()
     {
         $this->initAutoloader();
@@ -37,5 +45,33 @@ class Module
     public static function getConfig()
     {
         return new Config(include __DIR__ . '/configs/config.php');
+    }
+    
+    public function registerApplicationListeners(EventCollection $events, Locator $locator, Config $config)
+    {
+        $view          = $locator->get('view');
+        $viewListener  = $this->getViewListener($view, $config, $locator);
+        $events->attachAggregate($viewListener);
+    }
+
+    public function registerStaticListeners(StaticEventCollection $events, Locator $locator, Config $config)
+    {
+        $view         = $locator->get('view');
+        $viewListener = $this->getViewListener($view, $config, $locator);
+        $viewListener->registerStaticListeners($events, $locator);
+    }
+
+    protected function getViewListener($view, $config, $locator)
+    {
+        if ($this->viewListener instanceof View\Listener) {
+            return $this->viewListener;
+        }
+
+        $viewListener       = new View\Listener($view, $config->layout);
+        $viewListener->setDisplayExceptionsFlag($config->display_exceptions);
+        $viewListener->setDiContainer($locator);
+        
+        $this->viewListener = $viewListener;
+        return $viewListener;
     }
 }
