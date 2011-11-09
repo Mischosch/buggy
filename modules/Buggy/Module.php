6ca565e7aac8298301f4a7063245273757f02676
Leander <mail@leander-damme.com>
2011-11-09 23:26:09 +0100

    added Application Module
    lalalalaaa

---
+++ modules/Buggy/Module.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Buggy;
+
+use Zend\Module\Manager,
+    Zend\EventManager\StaticEventManager,
+    Zend\Loader\AutoloaderFactory;
+
+class Module
+{
+    protected $view;
+    protected $viewListener;
+
+    public function init(Manager $moduleManager)
+    {
+        $this->initAutoloader();
+        $events = StaticEventManager::getInstance();
+        $events->attach('bootstrap', 'bootstrap', array($this, 'initializeView'), 100);
+    }
+
+    protected function initAutoloader()
+    {
+        AutoloaderFactory::factory(array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        ));
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/configs/module.config.php';
+    }
+    
+    public function initializeView($e)
+    {
+        $app          = $e->getParam('application');
+        $locator      = $app->getLocator();
+        $config       = $e->getParam('modules')->getMergedConfig();
+        $view         = $this->getView($app);
+        $viewListener = $this->getViewListener($view, $config);
+        $app->events()->attachAggregate($viewListener);
+        $events       = StaticEventManager::getInstance();
+        $viewListener->registerStaticListeners($events, $locator);
+    }
+
+    protected function getViewListener($view, $config)
+    {
+        if ($this->viewListener instanceof View\Listener) {
+            return $this->viewListener;
+        }
+
+        $viewListener       = new View\Listener($view, $config->layout);
+        $viewListener->setDisplayExceptionsFlag($config->display_exceptions);
+
+        $this->viewListener = $viewListener;
+        return $viewListener;
+    }
+
+    protected function getView($app)
+    {
+        if ($this->view) {
+            return $this->view;
+        }
+
+        $di     = $app->getLocator();
+        $view   = $di->get('view');
+        $url    = $view->plugin('url');
+        $url->setRouter($app->getRouter());
+
+        $view->plugin('headTitle')->setSeparator(' - ')
+                                  ->setAutoEscape(false)
+                                  ->append('Application');
+        $this->view = $view;
+        return $view;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,39 +1,30 @@
 <?php
-
-// Define base path
-defined('BASE_PATH')
-    || define('BASE_PATH', realpath(__DIR__ . '/../'));
-    
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(__DIR__ . '/../application'));
-
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
-// Ensure library/ is on include_path
+// Ensure ZF is on the include path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
+    realpath(__DIR__ . '/../library'),
+    realpath(__DIR__ . '/../library/ZendFramework/library'),
     get_include_path(),
 )));
 
-// Autoloader
 require_once 'Zend/Loader/AutoloaderFactory.php';
-Zend\Loader\AutoloaderFactory::factory(array(
-    /*'Zend\Loader\ClassMapAutoloader' => array(
-        __DIR__ . '/../library/.classmap.php',
-        __DIR__ . '/../application/.classmap.php',
-    ),*/
-    'Zend\Loader\StandardAutoloader' => array(
-        'fallback_autoloader' => true,
-    ),
-));
+Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
+
+$appConfig = new Zend\Config\Config(include __DIR__ . '/../configs/application.config.php');
+
+$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
+$moduleLoader->register();
+
+$moduleManager = new Zend\Module\Manager(
+    $appConfig['modules'],
+    new Zend\Module\ManagerOptions($appConfig['module_manager_options'])
+);
 
 // Create application, bootstrap, and run
-$application = new Zend\Application\Application (
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-$application->bootstrap()
-            ->run();
+$bootstrap      = new Zend\Mvc\Bootstrap($moduleManager);
+$application    = new Zend\Mvc\Application;
+$bootstrap->bootstrap($application);
+$application->run()->send();
