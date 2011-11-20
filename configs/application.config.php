64d94f9d6734a228ba362139bbea9258d2ed03cf
Mischosch <michael@sch0ll.de>
2011-11-20 15:51:38 +0100

    resort modules initialization to fix DoctrineExtensions setup

--- configs/application.config.php
+++ configs/application.config.php
@@ -1,24 +1,24 @@
 <?php
 return array(
     'module_paths' => array(
-        realpath(__DIR__ . '/../modules'),
-    ),
+        realpath(__DIR__ . '/../modules')
+    ), 
     'modules' => array(
-    	'ZfComposer',
-        'Buggy',
-    	'SpiffyDoctrine',
-    	'SpiffyDoctrineExtensions',
-        'ZfCouchDb',
-    	'BuggyAdmin',
-		'EdpCommon',
-    	'EdpUser',
-    	'EdpMarkdown',
-    ),
-    'module_listener_options' => array( 
-        'config_cache_enabled'      => false,
-        'cache_dir'                => realpath(__DIR__ . '/../data/cache'),
+        'ZfComposer', 
+        'SpiffyDoctrine', 
+        'SpiffyDoctrineExtensions', 
+        'ZfCouchDb', 
+        'Buggy', 
+        'BuggyAdmin', 
+        'EdpCommon', 
+        'EdpUser', 
+        'EdpMarkdown'
+    ), 
+    'module_listener_options' => array(
+        'config_cache_enabled' => false, 
+        'cache_dir'            => realpath(__DIR__ . '/../data/cache')
         //'enable_dependency_check'  => false,
         //'enable_auto_installation' => false,
         //'manifest_dir'             => realpath(__DIR__ . '/../data'),
-    ),
+    )
 );
