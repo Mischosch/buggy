fe7024fdd5e31170b5805a3f8407d6e7ab56d31b
Mischosch <michael@sch0ll.de>
2011-11-20 15:51:52 +0100

    add development config

---
+++ modules/Buggy/configs/config.development.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'display_exceptions' => 1,
+    'di' => array(
+        'instance' => array(
+      		'doctrine_connection' => array(
+                'parameters' => array(
+                    'params' => array(
+                        'driver'   => 'pdo_mysql',
+                        'host'     => 'localhost',
+                        'port'     => '3306', 
+                        'user'     => 'root',
+                        'password' => '',
+                        'dbname'   => 'buggy',
+                    ),
+                )
+            ),
+        ),
+    ),
+);
