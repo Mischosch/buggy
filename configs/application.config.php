adcaf5d08965f8ba2013809246b7f9d85b9e7151
Mischosch <michael@sch0ll.de>
2011-11-26 02:50:21 +0100

    whitespaces & set application env by injecting it into listener

--- configs/application.config.php
+++ configs/application.config.php
@@ -2,21 +2,22 @@
 return array(
     'module_paths' => array(
         realpath(__DIR__ . '/../modules')
-    ), 
+    ),
     'modules' => array(
-        'ZfComposer', 
-        'SpiffyDoctrine', 
-        'SpiffyDoctrineExtensions', 
-        'ZfCouchDb', 
-        'Buggy', 
-        'BuggyAdmin', 
-        'EdpCommon', 
-        'EdpUser', 
+        'ZfComposer',
+        'SpiffyDoctrine',
+        'SpiffyDoctrineExtensions',
+        'ZfCouchDb',
+        'Buggy',
+        'BuggyAdmin',
+        'EdpCommon',
+        'EdpUser',
         'EdpMarkdown'
-    ), 
+    ),
     'module_listener_options' => array(
-        'config_cache_enabled' => false, 
-        'cache_dir'            => realpath(__DIR__ . '/../data/cache')
+        'config_cache_enabled'    => false,
+        'cache_dir'               => realpath(__DIR__ . '/../data/cache'),
+        'application_environment' => getenv('APPLICATION_ENV')
         //'enable_dependency_check'  => false,
         //'enable_auto_installation' => false,
         //'manifest_dir'             => realpath(__DIR__ . '/../data'),
